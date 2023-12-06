9ad6e79e8f73577ba38504cbdd30a320c32e95b4
JonathanMPeres <emailmarceloperes@gmail.com>
2023-12-06 15:38:13 -0300

    Initial Commit

---
+++ CSMemory/CSMemoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  CSMemoryApp.swift
+//  CSMemory
+//
+//  Created by Jonathan Marcelo Peres on 06/12/23.
+//
+
+import SwiftUI
+
+@main
+struct CSMemoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CSMemory/ContentView.swift
@@ -0,0 +1,48 @@
+//
+//  ContentView.swift
+//  CSMemory
+//
+//  Created by Jonathan Marcelo Peres on 06/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+            CardView()
+            CardView()
+            CardView()
+            CardView()
+        }
+        .foregroundColor(.orange)
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct CardView: View {
+    
+    @State var isFaceUp = false
+    
+    var body: some View {
+        ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
+            if isFaceUp {
+                base.fill(Color.white)
+                base.strokeBorder(lineWidth: 2)
+                Text("👻").font(.largeTitle)
+            } else {
+                base.fill()
+            }
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
+        }
+    }
+}
